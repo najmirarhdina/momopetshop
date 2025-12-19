911154bca6e1c8f105a17a0a425f7be993727927
Agatha <agathapuspaadellia@gmail.com>
2025-12-19 17:06:59 +0700

    supplier

--- src/main/java/com/management/momopetshop/controller/SupplierController.java
+++ src/main/java/com/management/momopetshop/controller/SupplierController.java
@@ -1,5 +1,39 @@
 package com.management.momopetshop.controller;
 
+import com.management.momopetshop.model.Supplier;
+import com.management.momopetshop.service.SupplierService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/supplier")
 public class SupplierController {
-    //
+
+    private final SupplierService service;
+
+    public SupplierController(SupplierService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<Supplier> getAll() {
+        return service.getAll();
+    }
+
+    @PostMapping
+    public Supplier create(@RequestBody Supplier supplier) {
+        return service.save(supplier);
+    }
+
+    @PutMapping("/{id}")
+    public Supplier update(@PathVariable Integer id, @RequestBody Supplier supplier) {
+        return service.update(id, supplier);
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable Integer id) {
+        service.delete(id);
+        return "Supplier berhasil dihapus";
+    }
 }
